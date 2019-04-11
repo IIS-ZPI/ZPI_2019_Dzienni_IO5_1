88ad179f190dc5c94bdf4e48a6cd4f3ebdae4ce3
astepaniuk <truemob2@gmail.com>
2019-04-11 15:27:49 +0200

    Project initial commit

---
+++ src/com/global/Main.java
@@ -0,0 +1,9 @@
+package com.global;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Group name: ZPI_2019_Dzienni_IO5_1\nTeam Leader: badfilling\nRole: operations");
+    }
+}
