e539beda5faf5de7f1c2b27c48f039308ef45a8b
astepaniuk <truemob2@gmail.com>
2019-04-18 14:39:43 +0200

    printint my githubID added

--- src/com/global/Main.java
+++ src/com/global/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Group name: ZPI_2019_Dzienni_IO5_1\nTeam Leader: badfilling\nRole: operations");
+        System.out.println("Artur Stepaniuk - githubID: badfilling");
     }
 }
