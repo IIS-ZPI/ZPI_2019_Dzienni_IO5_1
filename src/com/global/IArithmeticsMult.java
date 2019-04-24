ab5170082e2db9c58a38eb1184fcec96730da3d6
KovalykVolodymyr <211646@edu.p.lodz.pl>
2019-04-24 22:33:08 +0200

    Added IAritheticsMult Interface and impement in Main

---
+++ src/com/global/IArithmeticsMult.java
@@ -0,0 +1,5 @@
+package com.global;
+
+public interface IArithmeticsMult {
+    double Multiplication(double A, double B);
+}
--- src/com/global/Main.java
+++ src/com/global/Main.java
@@ -1,14 +1,19 @@
 package com.global;
 
-public class Main {
+public class Main implements IArithmeticsMult {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         System.out.println("Group name: ZPI_2019_Dzienni_IO5_1\nTeam Leader: badfilling\nRole: operations");
         System.out.println("kmzarski");
         System.out.println("Mariusz1306");
         System.out.println("GitHub id KovalykVolodymyr");
         System.out.println("GitHub id EkoGroszek");
         System.out.println("Artur Stepaniuk - githubID: badfilling");
+    }
+
+    @Override
+    public double Multiplication(double A, double B) {
+        return A * B;
     }
 }
