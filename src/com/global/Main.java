a69630504b7ed1a026b4073a33f7f3594bc2cbb8
Mariusz Czajkowski <mariusz1306+gmail.com>
2019-04-25 04:00:42 +0200

    Added ArithmeticInterfaces.java and ArithmeticPower class in Main.java

--- src/com/global/Main.java
+++ src/com/global/Main.java
@@ -1,13 +1,32 @@
 package com.global;
 
-public class Main {
+public class Main{
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         System.out.println("Group name: ZPI_2019_Dzienni_IO5_1\nTeam Leader: badfilling\nRole: operations");
+        System.out.println("kmzarski");
         System.out.println("Mariusz1306");
         System.out.println("GitHub id KovalykVolodymyr");
         System.out.println("GitHub id EkoGroszek");
         System.out.println("Artur Stepaniuk - githubID: badfilling");
     }
 }
+
+class ArithmeticPower implements IArithmeticsPower {
+
+    @Override
+    public double Power(double A, double B) {
+        if (B == 0)
+            return 1;
+        double result = A;
+        for (int i = 1; i < Math.abs(B); i++)
+            result *= A;
+        if (B > 0)
+            return result;
+        else
+            return 1/result;
+    }
+}
+
+
