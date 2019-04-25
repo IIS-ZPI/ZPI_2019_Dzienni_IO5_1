b51f3f2d39ab53b56e0bb1d2355e346c7f139ca3
EkoGroszek <44210792+EkoGroszek@users.noreply.github.com>
2019-04-25 14:35:07 +0200

    Merge pull request #21 from IIS-ZPI/f-kmzarski-zad6
    add comment

--- src/com/global/Main.java
+++ src/com/global/Main.java
@@ -4,6 +4,7 @@ public class Main implements IArithmeticsAdd, IArithmeticsDiff, IArithmeticsPowe
 
     public static void main(String[] args) {
         // write your code here
+        // comment
         System.out.println("Group name: ZPI_2019_Dzienni_IO5_1\nTeam Leader: badfilling\nRole: operations");
         System.out.println("kmzarski");
         System.out.println("Mariusz1306");
