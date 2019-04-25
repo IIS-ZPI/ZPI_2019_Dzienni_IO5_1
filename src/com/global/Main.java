d5f1f3382741d5c37e2a18c91f568492a9834825
Kamil Żarski <kmzarski@gmail.com>
2019-04-25 11:37:26 +0200

    komentarz3

--- src/com/global/Main.java
+++ src/com/global/Main.java
@@ -20,6 +20,7 @@ public class Main implements IArithmeticsAdd, IArithmeticsDiff, IArithmeticsPowe
     @Override
     public double Addition(double A, double B) {
         return A + B;
+        // komentarz 3
     }
     @Override
     public double Multiplication(double A, double B) {
