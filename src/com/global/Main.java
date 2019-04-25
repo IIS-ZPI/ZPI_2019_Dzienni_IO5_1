e978ade75afc4de7830a6946c1b6489cc4643196
KovalykVolodymyr <211646@edu.p.lodz.pl>
2019-04-25 11:42:51 +0200

    Add new Commit zad5_3

--- src/com/global/Main.java
+++ src/com/global/Main.java
@@ -21,6 +21,7 @@ public class Main implements IArithmeticsAdd, IArithmeticsDiff, IArithmeticsPowe
     public double Addition(double A, double B) {
         return A + B;
     }
+    ///Commit for zad5_3 KovalykVolodymyr
     @Override
     public double Multiplication(double A, double B) {
         return A * B;
