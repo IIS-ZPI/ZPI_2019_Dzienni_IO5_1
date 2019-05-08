7b12f143ff0c36507d83b35f9c19fc2163fed7d5
astepaniuk <truemob2@gmail.com>
2019-05-08 23:21:30 +0200

    a failing test added

---
+++ src/test/java/controllers/ResultWindowControlerTest.java
@@ -0,0 +1,16 @@
+package controllers;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ResultWindowControlerTest {
+
+	@Test
+	public void IWillNotPass() {
+		Assert.assertTrue(true);
+		Assert.assertTrue(false);
+	}
+
+}
